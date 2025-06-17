fc45c9def28424ad57312b9decc8bdea27ccfe91
Kyle Thomas <kylejwthomas@gmail.com>
2025-06-17 22:24:04 +0900

    Added language files

--- client/src/i18n/i18n.js
+++ client/src/i18n/i18n.js
@@ -1,8 +1,8 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
-import en from "./i18n/en.json";
-import ja from "./i18n/ja.json";
+import en from "./locales/en.json";
+import ja from "./locales/ja.json";
 
 i18n
   .use(LanguageDetector)
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div style={{ padding: "2rem", fontFamily: "Arial, sans-serif" }}>
+      <h1>Tahitian Dance School</h1>
+      <p>Welcome to our Tahitian dance school! We offer classes for all ages and skill levels.</p>
+      <p>Join us to learn the beautiful art of Tahitian dance and immerse yourself in the culture.</p>
+    </div>
+  );
+}

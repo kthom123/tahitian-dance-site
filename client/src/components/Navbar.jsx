967295987adc41a0eb26c04e6a6379b04e29872f
Kyle Thomas <kylejwthomas@gmail.com>
2025-06-17 22:34:33 +0900

    Updated Navbar

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <nav className="w-full px-4 py-3 flex justify-between items-center shadow-md bg-white">
+        <div className="text-2xl font-bold">Kei's Dance School
+
+        </div>
+        <ul className="flex gap-6 text-lg">
+        {/* Navigation Links */}
+        </ul>
+    </nav>
+  );
+}
+
+export default Navbar;

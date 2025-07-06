fc14e233c3c3cced705528ad0ac231a23b723cf5
Kyle Thomas <kylejwthomas@gmail.com>
2025-07-06 14:36:01 +0900

    Updated navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,18 +4,19 @@ import "./Navbar.css";
 function Navbar() {
   return (
     <nav className="w-full flex items-center justify-between px-6 py-4 shadow-md bg-white">
-      <div className="text-2xl font-bold">
-        Avae Api
-      </div>
-      <ul className="flex gap-6 text-lg font-medium justify-right">
-        <li><a href="#home" className="hover:text-blue-600 transition">Home</a></li>
-        <li><a href="#about" className="hover:text-blue-600 transition">About Us</a></li>
-        <li><a href="#classes" className="hover:text-blue-600 transition">Classes</a></li>
-        <li><a href="#instructor" className="hover:text-blue-600 transition">Instructor</a></li>
-        <li><a href="#events" className="hover:text-blue-600 transition">Events</a></li>
-        <li><a href="#trial lesson" className="hover:text-blue-600 transition">Trial Lesson</a></li>
-        <li><a href="#contact" className="hover:text-blue-600 transition">Contact</a></li>
-      </ul>
+        <div className="logo-title">
+            <img src="/images/logo.png" alt="Logo" className="logo-img" />
+            <span className="school-name">Avae Api</span>
+        </div>
+        <ul className="flex gap-6 text-lg font-medium">
+            <li><a href="#home" className="hover:text-blue-600 transition">Home</a></li>
+            <li><a href="#about" className="hover:text-blue-600 transition">About Us</a></li>
+            <li><a href="#classes" className="hover:text-blue-600 transition">Classes</a></li>
+            <li><a href="#instructor" className="hover:text-blue-600 transition">Instructor</a></li>
+            <li><a href="#events" className="hover:text-blue-600 transition">Events</a></li>
+            <li><a href="#trial lesson" className="hover:text-blue-600 transition">Trial Lesson</a></li>
+            <li><a href="#contact" className="hover:text-blue-600 transition">Contact</a></li>
+        </ul>
     </nav>
   );
 }

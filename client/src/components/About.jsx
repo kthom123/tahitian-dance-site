e1fe5ff6f73f016cd9a2c0304c5681cb0a10f0b8
Kyle Thomas <kylejwthomas@gmail.com>
2025-07-06 15:52:26 +0900

    Added About image

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
+import './About.css';
 
 function About() {
   return (
-    <section id="about" style={{ padding: '4rem 2rem', maxWidth: '1200px', margin: '0 auto' }}>
-        <h2 style={{ fontSize: '2.5rem', fontWeight: '700', marginBottom: '1rem' }}>About Us</h2>
-        <p style={{ fontSize: '1.125rem', lineHeight: '1.6', color: '#444' }}>
-        Welcome to Avae Api, where we celebrate the rich tradition and passion of Tahitian dance.
-        Our experienced instructors guide dancers of all levels to express themselves through this beautiful art form.
-        </p>
+    <section id="about" className="about-section">
+      <h2 className="about-heading">
+        Welcome to Avae Api Tahitian Dance School
+      </h2>
+    <div className="about-content">
+        <img src="/images/about.png" alt="about-pic" className="about-img" />
+    </div>
     </section>
   );
 }

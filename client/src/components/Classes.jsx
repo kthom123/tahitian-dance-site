41456a5f5146092b8a52288d9d9a5f55cdaae9e2
Kyle Thomas <kylejwthomas@gmail.com>
2025-07-07 22:27:03 +0900

    Added Classes css and jsx

--- client/src/components/Classes.jsx
+++ client/src/components/Classes.jsx
@@ -1,25 +1,25 @@
 import React from "react";
-import "./Lessons.css";
+import "./Classes.css";
 
-function Lessons() {
+function Classes() {
   return (
-    <section className="lessons-section">
-      <h2 className="lessons-title">Our Lessons</h2>
-      <div className="lessons-grid">
+    <section className="classes-section">
+      <h2 className="classes-title">クラス紹介</h2>
+      <div className="classes-grid">
         {/* Card 1 */}
-        <div className="lesson-card">
+        <div className="class-card">
           <h3>___________</h3>
           <p>___________</p>
         </div>
 
         {/* Card 2 */}
-        <div className="lesson-card">
+        <div className="class-card">
           <h3>___________</h3>
           <p>___________</p>
         </div>
 
         {/* Card 3 */}
-        <div className="lesson-card">
+        <div className="class-card">
           <h3>___________</h3>
           <p>___________</p>
         </div>
@@ -28,4 +28,4 @@ function Lessons() {
   );
 }
 
-export default Lessons;
+export default Classes;

1a6d1b0f440a034ffa2dd46e2fd43fe0c95edbef
Kyle Thomas <kylejwthomas@gmail.com>
2025-07-31 20:47:46 +0900

    Instuctor changes

--- client/src/components/Instructor.jsx
+++ client/src/components/Instructor.jsx
@@ -8,27 +8,27 @@ function Instructor() {
       <div className="instructor-container">
         <div className="instructor-image-group">
             <img src="/images/instructor.png" alt="Instructor" className="instructor-photo" />
-            <div className="instructor-name-group">
-                <p className="instructor-name-jp">木月けい</p>
-                <p className="instructor-name-en">Kei Kizuki</p>        
-            </div>
         </div>
         <div className="instructor-bio">
+          <h3 className="instructor-name">Kei Kizuki</h3>
+          <br />
           <p>
             タヒチアンダンスの心揺さぶるドラムビート、人や自然への愛にあふれたタヒチ文化は、目に見えない大切なものに気づかせてくれました。
             現在は東京を拠点に活動していますが、2025年からは故郷・群馬県でも活動をスタートします。
             <br />
-            千葉大学卒業。教育業、観光業（南太平洋諸島担当）での勤務を経て、タヒチアンダンスを学び続けています。
             TIARE VAREAU所属、野口みどり先生に師事。数々のショー、イベント出演歴あり。
+            千葉大学卒業。教育業、観光業（南太平洋諸島担当）に従事。
+            <br />
             <br />
             ≪大会受賞歴≫
-            ・2020年 Heiva i Japan Piti 3位
-            ・2021年 Heiva i Tokyo Piti 優勝
-            ・2022年 Heiva i Tokyo Championship Toru 準優勝
+            <br />2020年 Heiva i Japan Piti 3位
+            <br />2021年 Heiva i Tokyo Piti 優勝
+            <br />2022年 Heiva i Tokyo Championship Toru 準優勝
+            <br />
             <br />
             ≪資格≫
-            ・温活士
-            ・健康ソムリエ
+            <br />温活士
+            <br />健康ソムリエ
           </p>
         </div>
       </div>
--- client/src/components/TrialLesson.jsx
+++ client/src/components/TrialLesson.jsx
@@ -7,8 +7,8 @@ function TrialLesson() {
       <h2 className="trial-title">体験レッスン</h2>
 
       <p className="trial-description">
-        体験レッスンご希望の方は、お問合せフォームよりご連絡をお待ちしております。<br />
-        経験・未経験問わず、新規生徒募集中です。プレオープン中のため、現在開講していないクラスのご相談も可能です。
+        体験レッスンご希望の方は、お問合せフォームよりご連絡ください。<br />
+        経験・未経験問わず、新規生徒様募集中です。現在開講していないクラスも、お気軽にご相談ください。
       </p>
 
       <div className="trial-items">
@@ -21,7 +21,6 @@ function TrialLesson() {
         </ul>
         <p className="trial-fee">
           体験レッスン費用：<strong>1,000円</strong><br />
-          ※体験レッスン当日のご入会で<strong>全額返金</strong>
         </p>
       </div>
     </section>

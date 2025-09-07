af9764f7a85e89f8d06f315e485045714ddaa316
Kyle Thomas <kylejwthomas@gmail.com>
2025-09-07 16:01:36 +0900

    updated navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./Navbar.css";
 
 function Navbar() {
@@ -6,7 +7,10 @@ function Navbar() {
   return (
     <nav>
       {/* Hamburger Button */}
-      <div className="hamburger" onClick={() => setOpen(!open)}>
+      <div
+        className={`hamburger ${open ? "active" : ""}`}
+        onClick={() => setOpen(!open)}
+      >
         <span></span>
         <span></span>
         <span></span>
@@ -15,13 +19,13 @@ function Navbar() {
       {/* Nav Links */}
       <div className={`nav-links ${open ? "active" : ""}`}>
         <ul>
-          <li><a href="#home">ホーム</a></li>
-          <li><a href="#about">教室紹介</a></li>
-          <li><a href="#classes">クラス紹介</a></li>
-          <li><a href="#instructor">講師紹介</a></li>
-          <li><a href="#events">イベント出演情報</a></li>
-          <li><a href="#trial-lesson">体験レッスン</a></li>
-          <li><a href="#contact">お問合せ</a></li>
+          <li><a href="#home" onClick={() => setOpen(false)}>ホーム</a></li>
+          <li><a href="#about" onClick={() => setOpen(false)}>教室紹介</a></li>
+          <li><a href="#classes" onClick={() => setOpen(false)}>クラス紹介</a></li>
+          <li><a href="#instructor" onClick={() => setOpen(false)}>講師紹介</a></li>
+          <li><a href="#events" onClick={() => setOpen(false)}>イベント出演情報</a></li>
+          <li><a href="#trial-lesson" onClick={() => setOpen(false)}>体験レッスン</a></li>
+          <li><a href="#contact" onClick={() => setOpen(false)}>お問合せ</a></li>
         </ul>
       </div>
     </nav>
